c0dec9e0423839d901f1ecd486faa643d7d5b04d
moorts <moritz.gutfleisch@gmx.de>
2023-12-03 14:31:40 +0100

    [Day3] Refactor

--- day3/solve.py
+++ day3/solve.py
@@ -1,58 +1,22 @@
 from collections import defaultdict
 
 def neighbors(y, x, n):
-    l = []
-    if y > 0:
-        l.append((y - 1, x))
-        if x < n - 1:
-            l.append((y - 1, x + 1))
-        if x > 0:
-            l.append((y - 1, x - 1))
-    if y < n - 1:
-        l.append((y + 1, x))
-        if x < n - 1:
-            l.append((y + 1, x + 1))
-        if x > 0:
-            l.append((y + 1, x - 1))
-    if x > 0:
-        l.append((y, x - 1))
-    if x < n - 1:
-        l.append((y, x + 1))
-    return l
+    for dy in [-1, 0, 1]:
+        for dx in [-1, 0, 1]:
+            ny = y + dy
+            nx = x + dx
+            if 0 < ny < n and 0 < nx < n:
+                yield (ny, nx)
 
-def part1(schematic):
-    n = len(schematic)
-    m = len(schematic[0])
-    assert n == m
-
-    out = 0
-    for y in range(n):
-        x = 0
-        while x < n:
-            part_num = False
-            value = 0
-            while x < n and schematic[y][x].isdigit():
-                value *= 10
-                value += int(schematic[y][x])
-
-                if not part_num:
-                    for ny, nx in neighbors(y, x, n):
-                        if schematic[ny][nx] not in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."]:
-                            part_num = True
-                x += 1
-            x += 1
-            if part_num:
-                out += value
-    return out
-
-def part2(schematic):
+def solve(schematic):
     n = len(schematic)
     m = len(schematic[0])
     assert n == m
 
     gears = defaultdict(list)
 
-    out = 0
+    part1 = 0
+    part2 = 0
     for y in range(n):
         x = 0
         while x < n:
@@ -63,23 +27,22 @@ def part2(schematic):
                 value *= 10
                 value += int(schematic[y][x])
 
-                if not part_num:
-                    for ny, nx in neighbors(y, x, n):
-                        if schematic[ny][nx] not in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."]:
-                            part_num = True
-                            if schematic[ny][nx] == "*":
-                                adj_gear = (ny, nx)
+                for ny, nx in neighbors(y, x, n):
+                    if schematic[ny][nx] not in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."]:
+                        part_num = True
+                        if schematic[ny][nx] == "*":
+                            adj_gear = (ny, nx)
 
                 x += 1
             x += 1
-            if part_num and adj_gear is not None:
-                gears[adj_gear].append(value)
+            if part_num:
+                part1 += value
+                if adj_gear is not None:
+                    gears[adj_gear].append(value)
     for l in gears.values():
         if len(l) == 2:
-            out += l[0] * l[1]
-    return out
-
-
+            part2 += l[0] * l[1]
+    return part1, part2
 
 
 
@@ -88,6 +51,5 @@ with open("./data") as f:
     data = f.read()
     lines = data.splitlines()
 
-    print(part1(lines))
-    print(part2(lines))
+    print(solve(lines))
 
